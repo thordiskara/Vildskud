8a759b7112a15c061af8b0d162d525b0575098e1
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-10-10 15:47:35 +0200

    program filtering

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+// PROGRAM FILTERING
+function ingenforestillinger(){
+    const skjulteforestillinger = document.querySelectorAll('.forestilling.hidden').length;
+    if(skjulteforestillinger === 7){
+      document.querySelector("#ingenforestillinger").classList.remove("hidden");
+    } else {
+      document.querySelector("#ingenforestillinger").classList.add("hidden");
+    }          
+  }  
