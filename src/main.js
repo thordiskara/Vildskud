9982047faa799d1a62317c70f90f86af999bd05c
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-10-11 14:03:20 +0200

    burger

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import "./style.css";
 
 // PROGRAM FILTERING
-function ingenforestillinger(){
-    const skjulteforestillinger = document.querySelectorAll('.forestilling.hidden').length;
-    if(skjulteforestillinger === 7){
-      document.querySelector("#ingenforestillinger").classList.remove("hidden");
-    } else {
-      document.querySelector("#ingenforestillinger").classList.add("hidden");
-    }          
-  }  
+// function ingenforestillinger(){
+//     const skjulteforestillinger = document.querySelectorAll('.forestilling.hidden').length;
+//     if(skjulteforestillinger === 7){
+//       document.querySelector("#ingenforestillinger").classList.remove("hidden");
+//     } else {
+//       document.querySelector("#ingenforestillinger").classList.add("hidden");
+//     }          
+//   }  
+
+// BURGER MENU
+const menuIcon = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+});
