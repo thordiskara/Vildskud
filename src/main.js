24149f9e80ee339af9b531e0dcfe356ae87f956a
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-10-16 17:10:12 +0200

    boks

--- src/main.js
+++ src/main.js
@@ -17,3 +17,20 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+//COOKIES
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks(){
+    cookieboks.classList.add("hidden");
+    sessionStorage.setItem("fjerncookieboks", true);
+}
+
+if(sessionStorage.getItem("fjerncookieboks")){
+    cookieboks.classList.add("hidden");
+}
