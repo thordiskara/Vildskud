84c1f175e72e40de301403d13ea33d1ec1e98076
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-10-16 18:03:42 +0200

    footer and cookie

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import "./style.css";
 
-// PROGRAM FILTERING
-// function ingenforestillinger(){
-//     const skjulteforestillinger = document.querySelectorAll('.forestilling.hidden').length;
-//     if(skjulteforestillinger === 7){
-//       document.querySelector("#ingenforestillinger").classList.remove("hidden");
-//     } else {
-//       document.querySelector("#ingenforestillinger").classList.add("hidden");
-//     }          
-//   }  
-
 // BURGER MENU
 const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
